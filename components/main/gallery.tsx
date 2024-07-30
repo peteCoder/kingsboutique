4ec6e45a43002a1dcb320fc8a4fcc809c0f89c56
peteCoder <petercodercoder@gmail.com>
2024-07-30 09:09:32 -0700

    added shadcn carousel

--- components/main/gallery.tsx
+++ components/main/gallery.tsx
@@ -2,31 +2,76 @@
 
 import { urlFor } from "@/lib/client";
 import { ProductSanitySchemaResult } from "@/types";
-import { Carousel, ThemeProvider } from "@material-tailwind/react";
+// import { Carousel, ThemeProvider } from "@material-tailwind/react";
 import React from "react";
 
+import Autoplay from "embla-carousel-autoplay"
+ 
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image";
+
+// const Gallery = ({ product }: { product: ProductSanitySchemaResult }) => {
+//   return (
+//     <ThemeProvider>
+//       <Carousel
+//         className="w-full h-[60vh]"
+//         navigation={({ setActiveIndex, activeIndex, length }) => {
+//           // Remove the navigation by returning null
+//           return null;
+//         }}
+//       >
+//         {product?.gallery?.map((product) => (
+//           <div
+//             key={product._id}
+//             style={{
+//               backgroundImage: `url(${urlFor(product?.imageUrl)})`,
+//             }}
+//             className="h-full w-full relative bg-cover bg-center bg-no-repeat"
+//           ></div>
+//         ))}
+//       </Carousel>
+//     </ThemeProvider>
+//   );
+// };
 const Gallery = ({ product }: { product: ProductSanitySchemaResult }) => {
+
+  // Continue from carousel
+
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+
   return (
-    <ThemeProvider>
-      <Carousel
-        className="w-full h-[60vh]"
-        navigation={({ setActiveIndex, activeIndex, length }) => {
-          // Remove the navigation by returning null
-          return null;
-        }}
+    <div className="">
+      <Carousel className=""
+      plugins={[plugin.current]}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
       >
-        {product?.gallery?.map((product) => (
-          <div
-            key={product._id}
-            style={{
-              backgroundImage: `url(${urlFor(product?.imageUrl)})`,
-            }}
-            className="h-full w-full relative bg-cover bg-center bg-no-repeat"
-          ></div>
-        ))}
+        <CarouselContent className="" >
+        {product?.gallery?.map((item) => (
+            <CarouselItem  key={item._id} >
+              <Card className="bg-center bg-transparent border-none">
+                  <CardContent className="flex aspect-square items-center justify-center p-0 bg-center">
+                    <Image src={`${urlFor(item?.imageUrl)}`} alt="" width={600} height={600} className="w-[80%] mx-auto"/>
+                  </CardContent>
+                </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="left-0 top-1/2 -translate-y-1/2" />
+        <CarouselNext className="right-0 top-1/2 -translate-y-1/2" />
       </Carousel>
-    </ThemeProvider>
-  );
+    </div>
+    
+  )
 };
 
 export default Gallery;
