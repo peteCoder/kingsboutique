56a3beb1e4c544aea2b517fd6277f39a5c96c6e5
peteCoder <talk2peteresezobor@gmail.com>
2025-01-14 16:53:10 +0100

    Added 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,12 +3,11 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div className='bg-[#2563eb] min-h-screen flex justify-center items-center'>
-        
+    <div className='dark:bg-[#181412] bg-primary min-h-screen flex justify-center items-center p-2'>
         <div className="text-white">
         <h2 className='text-white font-extrabold text-6xl md:text-8xl'>404</h2>
         <h2 className='font-bold'> <span className='text-2xl block mb-2'>Ooops! </span> <span className='mt-2'>Could not find what you are looking for?</span></h2>
-        <Link href="/" className='bg-transparent border-white border text-white hover:text-black hover:bg-white transition-all duration-200  p-3 inline-block rounded-sm mt-2'>Go back Home</Link>
+        <Link href="/" className='bg-transparent border-white border text-white hover:text-black hover:bg-white transition-all duration-200  p-3 inline-block rounded-sm mt-2'>Go back</Link>
         </div>
     </div>
   )
